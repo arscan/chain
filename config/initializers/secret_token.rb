e5294cdaa36e43659528f9d96f7e2e820411b340
Rob Scanlon <robscanlon@gmail.com>
2013-04-25 20:11:48 -0400

    adding stuff generated by rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chain::Application.config.secret_token = 'bc24bccf7c8f63f0c1724c765cceb2886bd101ab3c18d74e76c0d1b6681367ef037f5be961308bbe4e96b629881a69b0ee7e769e3b7012a4a0a2c9e4d9cd3ebd'
