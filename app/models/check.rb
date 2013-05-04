f4279a62036bbdcd0d43be5c0227d0781e939b0e
Rob Scanlon <robscanlon@gmail.com>
2013-05-03 20:22:21 -0400

    added some tests to the check model

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,4 +1,6 @@
 class Check < ActiveRecord::Base
   belongs_to :activity
   attr_accessible :datetime
+
+  validates :datetime, presence: true
 end
--- test/unit/check_test.rb
+++ test/unit/check_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CheckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "should be able to create a check" do
+        activity = Activity.new(name: "check test")
+        activity.save
+        check = activity.checks.create(datetime: Time.now)
+        assert check.valid?
+    end
+
+    test "datetime cannot be blank" do
+        activity = Activity.new(name: "check test2")
+        activity.save
+        check = activity.checks.create
+        assert check.invalid?
+        assert check.errors[:datetime].any?
+    end
+    
 end
