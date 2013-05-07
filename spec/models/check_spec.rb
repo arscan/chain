7217fffae2bce1008cc6486b7e059e0365b4b8c5
Rob Scanlon <robscanlon@gmail.com>
2013-05-06 23:01:37 -0400

    removed debug line

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -7,7 +7,6 @@ describe Check do
         it "cannot have an empty date" do
 
             activity = create(:activity_with_check)
-            puts activity.checks.inspect
             activity.checks[0].datetime = nil
 
             activity.checks[0].should_not be_valid
